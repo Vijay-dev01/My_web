481e2925dc275d52ad9c621e2e18d3f9b895c8ca
Vijay-dev01 <vijayajay3535@gmail.com>
2023-09-08 21:45:43 +0530

    Hosting link updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,54 +5,62 @@ const Footer = (props) => {
     return (
         <>
             <div >
-            <div className={`Footer bg-${props.mode === 'light' ? 'white' : 'dark'}
+                <div className={`Footer bg-${props.mode === 'light' ? 'white' : 'dark'}
     text-${props.mode === 'light' ? 'white' : 'dark'}`} >
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6 col-lg-5 col-12 ft-1">
-                            <h3><span>Vijay</span>CODES</h3>
-                            <p className={`text-${props.mode === 'light' ? 'dark' : 'white'}`}>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum ea quo ex
-                                ullam laboriosam magni totam, facere eos iure voluptate.</p>
-                            <div className="footer-icons">
-                                <i class="fa-brands fa-facebook"></i>
-                                <i class="fa-brands fa-twitter"></i>
-                                <i class="fa-brands fa-instagram"></i>
-                                <i class="fa-brands fa-linkedin-in"></i>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-6 col-lg-5 col-12 ft-1">
+                                <h3><span>Vijay</span>CODES</h3>
+                                <p className={`text-${props.mode === 'light' ? 'dark' : 'white'}`}>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum ea quo ex
+                                    ullam laboriosam magni totam, facere eos iure voluptate.</p>
+                                <div className="footer-icons">
+                                    <a href="https://github.com/Vijay-dev01" target="_blank" rel="noopener noreferrer">
+                                        <i class="fa-brands fa-github" />
+                                    </a>
+                                    <a href="https://instagram.com/thev_i_j_a_y?utm_source=qr&igshid=MzNlNGNkZWQ4Mg==" target="_blank" rel="noopener noreferrer">
+                                        <i class="fa-brands fa-twitter" />
+                                    </a>
+                                    <a href="https://instagram.com/thev_i_j_a_y?utm_source=qr&igshid=MzNlNGNkZWQ4Mg==" target="_blank" rel="noopener noreferrer">
+                                        <i class="fa-brands fa-instagram" />
+                                    </a>
+                                    <a href="https://www.linkedin.com/in/vijay-g-9667ab243" target="_blank" rel="noopener noreferrer">
+                                        <i class="fa-brands fa-linkedin-in" />
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-md-6 col-lg-3 col-12 ft-2">
-                            <h5>Quick Links</h5>
-                            <ul>
-                                <li className="nav-item">
-                                    <Link className={`text-${props.mode === 'light' ? 'dark' : 'white'}`} href="/">Services</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className={`text-${props.mode === 'light' ? 'dark' : 'white'}`} href="/">Portfolio</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className={`text-${props.mode === 'light' ? 'dark' : 'white'}`} href="/">Contact Us</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className={`text-${props.mode === 'light' ? 'dark' : 'white'}`} href="/">Services</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className={`text-${props.mode === 'light' ? 'dark' : 'white'}`} href="/">Portfolio</Link>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="col-md-6 col-lg-4 col-12 ft-3">
-                            <h5>Quick Links</h5>
-                            <p className={`text-${props.mode === 'light' ? 'dark' : 'white'}`}><i className="fa-solid fa-phone-volume"></i> +91 9148505856</p>
-                            <p className={`text-${props.mode === 'light' ? 'dark' : 'white'}`}><i className="fa-solid fa-envelope"></i> vijayajay3535@gmail.com</p>
-                            <p className={`text-${props.mode === 'light' ? 'dark' : 'white'}`}><i className="fa-solid fa-paper-plane"></i> Mysore, karnataka.</p>
+                            <div className="col-md-6 col-lg-3 col-12 ft-2">
+                                <h5>Quick Links</h5>
+                                <ul>
+                                    <li className="nav-item">
+                                        <Link className={`text-${props.mode === 'light' ? 'dark' : 'white'}`} href="/">Services</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className={`text-${props.mode === 'light' ? 'dark' : 'white'}`} href="/">Portfolio</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className={`text-${props.mode === 'light' ? 'dark' : 'white'}`} href="/">Contact Us</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className={`text-${props.mode === 'light' ? 'dark' : 'white'}`} href="/">Services</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className={`text-${props.mode === 'light' ? 'dark' : 'white'}`} href="/">Portfolio</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-md-6 col-lg-4 col-12 ft-3">
+                                <h5>Quick Links</h5>
+                                <p className={`text-${props.mode === 'light' ? 'dark' : 'white'}`}><i className="fa-solid fa-phone-volume"></i> +91 9148505856</p>
+                                <p className={`text-${props.mode === 'light' ? 'dark' : 'white'}`}><i className="fa-solid fa-envelope"></i> vijayajay3535@gmail.com</p>
+                                <p className={`text-${props.mode === 'light' ? 'dark' : 'white'}`}><i className="fa-solid fa-paper-plane"></i> Mysore, karnataka.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className='Last-footer'>
-                <p className='text-white'>Design By Vijay Ganesh</p>
-            </div>
+                <div className='Last-footer'>
+                    <p className='text-white'>Design By Vijay Ganesh</p>
+                </div>
             </div>
         </>
     )
